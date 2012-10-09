1abd5c406b2da6c4632c3aed6a2233026c4c4855
Brad Rydzewski <brad.rydzewski@gmail.com>
2012-10-08 17:58:16 -0700

    fixed issue where posts not in reverse chronological order

--- site.go
+++ site.go
@@ -156,7 +156,8 @@ func (s *Site) read() error {
 		case isPost(rel) :
 			post, err := ParsePost(rel)
 			if err != nil { return err }
-			s.posts = append(s.posts, post)
+			// TODO: this is a hack to get the posts in rev chronological order
+			s.posts = append([]Page{post}, s.posts...)//s.posts, post)
 
 		// Parse Pages
 		case isPage(rel) :
